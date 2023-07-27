d64e26368f86fd44ac06202f95083127c6136dcb
Natasa410 <natasa410@hotmail.com>
2023-07-27 12:22:06 +0200

    remove template - add search bar with static data

---
+++ lib/book_author_search.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'book_author_search_delegate.dart';
+
+class BookAuthorSearch extends StatefulWidget {
+  const BookAuthorSearch({Key? key}) : super(key: key);
+
+  @override
+  State<BookAuthorSearch> createState() => _BookAuthorSearch();
+}
+
+class _BookAuthorSearch extends State<BookAuthorSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "GeeksForGeeks",
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // method to show the search bar
+              showSearch(
+                  context: context,
+                  // delegate to customize the search bar
+                  delegate: BookAuthorSearchDelegate());
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/book_author_search_delegate.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class BookAuthorSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = ['moby dick', 'animal farm', 'the Hobbit'];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var author in searchTerms) {
+      if (author.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(author);
+      }
+    }
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+      itemCount: matchQuery.length,
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var author in searchTerms) {
+      if (author.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(author);
+      }
+    }
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+      itemCount: matchQuery.length,
+    );
+  }
+}
